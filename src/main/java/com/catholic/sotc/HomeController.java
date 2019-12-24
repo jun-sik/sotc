253a3171d547c3e350c059b3aa2ac6205d0cbd96
Dev212 <Dev212@DESKTOP-AQE7O4Q>
2019-12-24 17:37:28 +0900

    testestest

--- src/main/java/com/catholic/sotc/HomeController.java
+++ src/main/java/com/catholic/sotc/HomeController.java
@@ -1,17 +1,11 @@
 package com.catholic.sotc;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,9 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@Autowired
-	BasicDataSource dataSource;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -41,38 +32,6 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
-	
-	@RequestMapping("/dbTest.do")
-	public String dbTest(Model model) {
-		Connection conn = null;
-		Statement st = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			st = conn.createStatement();
-			ResultSet rs = st.executeQuery("select now() as now;");
-			
-			while ( rs.next() ) {
-				model.addAttribute("serverTime", rs.getString("now"));
-			}
-		}
-		catch ( Exception e ) {
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				if ( conn != null ) {
-					conn.close();
-				}
-			}
-			catch ( SQLException e ) {
-				e.printStackTrace();
-			}
-		}
 		
 		return "home";
 	}
